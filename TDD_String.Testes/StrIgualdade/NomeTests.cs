549f04f415ffcfaba73b0aef6d2d6447a2bee4dc
Rafael Batista <maranata_rafa@hotmail.com>
2019-05-20 01:23:53 -0400

    Added tests string

---
+++ TDD_String.Testes/StrIgualdade/NomeTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace TDD_String.Tests.StrIgualdade
+{
+    [TestFixture]
+    public class NomeTests
+    {
+        [Test]
+        //Testa junção de nomes
+        public void DevoJuntarDuasStrings()
+        {
+            var sut = new Nome();
+            var nomeCompleto = sut.JuntarNomes("Rafael", "Batista");
+
+            Assert.That(nomeCompleto, Is.EqualTo("Rafael Batista"));
+        }
+        [Test]
+        //Testa nomes com Case Sensitive
+        public void DevoJuntarDuasStrings_CaseSensitive()
+        {
+            var sut = new Nome();
+            var nomeCompleto = sut.JuntarNomes("rafael", "BATISTA");
+
+            Assert.That(nomeCompleto, Is.EqualTo("Rafael Batista").IgnoreCase);
+        }
+        [Test]
+        //Testa nomes diferentes
+        public void DevoJuntarDuasStringsDiferentes()
+        {
+            var sut = new Nome();
+            var nomeCompleto = sut.JuntarNomes("Rafael", "Batista");
+
+            Assert.That(nomeCompleto, Is.Not.EqualTo("Tatiane Roque"));
+        }
+    }
+}
