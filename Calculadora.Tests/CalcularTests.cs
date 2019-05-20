022eed9351347c8aa308bab5b2c3160991211e41
Rafael Batista <maranata_rafa@hotmail.com>
2019-05-20 00:15:53 -0400

    Add method Multiplicar - Tested

--- Calculadora.Tests/CalcularTests.cs
+++ Calculadora.Tests/CalcularTests.cs
@@ -11,7 +11,14 @@ namespace Calculadora.Tests
             var resultado = sut.Somar(5, 5);
 
             Assert.That(resultado, Is.EqualTo(10));
-            
+        }
+        [Test]
+        public void DeveMultiplicarDoisNumeros()
+        {
+            var sut = new Calcular();
+            var resultado = sut.Multiplicar(3, 2);
+
+            Assert.That(resultado, Is.EqualTo(6));
         }
     }
 }
--- Calculadora/Calcular.cs
+++ Calculadora/Calcular.cs
@@ -8,9 +8,13 @@ namespace Calculadora
 {
     public class Calcular
     {
-        //public int Somar(int n1, int n2)
-        //{
-        //    return 0;
-        //}
+        public int Somar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public int Multiplicar(int n1, int n2)
+        {
+            return n1 * n2;
+        }
     }
 }
